89e3d0f4079f7496b21510efe1a42373abef8b1b
k1LoW <k1lowxb@gmail.com>
2011-01-08 17:10:04 +0900

    Add 'Smart Replace' action

--- anything-replace-string.el
+++ anything-replace-string.el
@@ -1,4 +1,4 @@
-;;; anything-replace-string.el --- `replace-string' `anything.el' interface
+;;; anything-replace-string.el --- `replace-string' and `query-replace' `anything.el' interface
 ;; -*- Mode: Emacs-Lisp -*-
 
 ;; Copyright (C) 2011 by 101000code/101000LAB
@@ -17,8 +17,10 @@
 ;; along with this program; if not, write to the Free Software
 ;; Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
 
-;; Version: 0.5.0
+;; Version: 0.8.0
 ;; Author: k1LoW (Kenichirou Oyama), <k1lowxb [at] gmail [dot] com> <k1low [at] 101000lab [dot] org>
+;; Maintainer: k1LoW (Kenichirou Oyama), <k1lowxb [at] gmail [dot] com> <k1low [at] 101000lab [dot] org>
+;;             kitokitoki
 ;; URL: http://code.101000lab.org
 
 ;;; Install
@@ -52,7 +54,7 @@
 (require 'anything)
 
 (defgroup anything-replace-string nil
-  "`replae-string' `anything.el' interface"
+  "`replae-string' and `query-replae' `anything.el' interface"
   :group 'lisp
   :prefix "anything-replace-string-")
 
@@ -73,22 +75,23 @@
   :group 'anything-replace-string)
 
 (defadvice replace-string (before anything-replace-string-replace-string(from-string to-string &optional delimited start end) activate)
-  (anything-replace-string-push-history from-string to-string))
+  (anything-replace-string-push-history from-string to-string 'replace-string))
 
 (defadvice query-replace (before anything-replace-string-query-replace(from-string to-string &optional delimited start end) activate)
-  (anything-replace-string-push-history from-string to-string))
+  (anything-replace-string-push-history from-string to-string 'query-string))
 
-(defun anything-replace-string-push-history (from-string to-string)
+(defun anything-replace-string-push-history (from-string to-string &optional replace-type)
   "Push replace history."
-  (push (cons from-string to-string) anything-replace-string-history)
+  (push (list from-string to-string replace-type) anything-replace-string-history)
   (push (concat from-string anything-replace-string-separator to-string) anything-replace-string-history-candidates))
 
 (defvar anything-c-source-replace-string
   '((name . "Replace string from history")
     (candidates . anything-replace-string-history-candidates)
     (action
-       ("Replace String" . anything-replace-string-action)
-       ("Query Replace" . anything-query-replace-action))
+     ("Smart Replace" . anything-smart-replace-action)
+     ("Replace String" . anything-replace-string-action)
+     ("Query Replace" . anything-query-replace-action))
     (migemo)
     (multiline)))
 
@@ -99,58 +102,77 @@
      ("Replace String" . anything-replace-string-dummy-action)
      ("Query Replace" . anything-query-replace-dummy-action))))
 
-(defun anything-replace-string-action (candidate)
+(defun anything-smart-replace-action (candidate)
   (loop with match = nil
         until match
         for x in anything-replace-string-history
-        do (if (equal (concat (car x) anything-replace-string-separator (cdr x)) candidate)
+        do (if (equal (concat (car x) anything-replace-string-separator (cadr x)) candidate)
+               (progn
+                 (cond ((equal 'replace-string (caddr x)) (anything-replace-string-region x))
+                       ((equal 'query-string (caddr x)) (anything-query-replace-region-region x))
+                       (t (anything-replace-string-region x)))
+                 (setq match t)
+                 (return nil)))))
+
+(defun anything-replace-string-action (candidate)
+  (message "replace")
+  (loop with match = nil
+        until match
+        for x in anything-replace-string-history
+        do (if (equal (concat (car x) anything-replace-string-separator (cadr x)) candidate)
                (progn
                  (anything-replace-string-region x)
                  (setq match t)
                  (return nil)))))
 
 (defun anything-query-replace-action (candidate)
+  (message "query")
   (loop with match = nil
         until match
         for x in anything-replace-string-history
-        do (if (equal (concat (car x) anything-replace-string-separator (cdr x)) candidate)
+        do (if (equal (concat (car x) anything-replace-string-separator (cadr x)) candidate)
                (progn
                  (anything-query-replace-region x)
                  (setq match t)
                  (return nil)))))
 
 (defun anything-replace-string-dummy-action (candidate)
-   (let ((to-string candidate) (prompt "Replace string in region "))
-     (unless (region-active-p)
-       (setq prompt "Replace string "))
-     (setq to-string (read-string (concat prompt candidate " with: ")))
-     (anything-replace-string-region (cons candidate to-string))
-     (anything-replace-string-push-history candidate to-string)))
+  (let ((to-string candidate) (prompt "Replace string in region "))
+    (unless (region-active-p)
+      (setq prompt "Replace string "))
+    (setq to-string (read-string (concat prompt candidate " with: ")))
+    (anything-replace-string-push-history candidate to-string 'replace-string)
+    (anything-replace-string-region (list candidate to-string 'replace-string))))
 
 (defun anything-query-replace-dummy-action (candidate)
-   (let ((to-string candidate) (prompt "Query Replace string in region "))
-     (unless (region-active-p)
-       (setq prompt "Query Replace string "))
-     (setq to-string (read-string (concat prompt candidate " with: ")))
-     (anything-replace-string-push-history candidate to-string)
-     (anything-query-replace-region (cons candidate to-string))))
+  (let ((to-string candidate) (prompt "Query Replace string in region "))
+    (unless (region-active-p)
+      (setq prompt "Query Replace string "))
+    (setq to-string (read-string (concat prompt candidate " with: ")))
+    (anything-replace-string-push-history candidate to-string 'query-string)
+    (anything-query-replace-region (list candidate to-string 'query-string))))
 
 (defun anything-replace-string-region (x)
   "Replace string."
-  (let ((count 0) (current (point)) (beginning (region-beginning)) (end (region-end)))
+  (let ((from-string (car x))
+       (to-string (cadr x))
+       (count 0)
+       (current (point))
+       (beginning (region-beginning))
+       (end (region-end)))
     (if (region-active-p)
         (progn
           (goto-char beginning)
-          (while (search-forward (car x) nil t)
+          (while (search-forward from-string nil t)
             (unless (< end (point))
               (incf count)
-              (replace-match (cdr x) nil t)
+              (replace-match from-string nil t)
               (unless (< end (point))
                 (setq current (point))))))
       (goto-char (point-min))
-      (while (search-forward (car x) nil t)
+      (while (search-forward from-string nil t)
         (incf count)
-        (replace-match (cdr x) nil t)
+        (replace-match to-string nil t)
         (setq current (point))))
     (goto-char current)
     (message (concat "Replaced " (number-to-string count) " occurrences"))
@@ -159,7 +181,7 @@
 (defun anything-query-replace-region (x)
   "Query Replace string."
   (let((from-string (car x))
-       (to-string (cdr x)))
+       (to-string (cadr x)))
     (if (region-active-p)
         (perform-replace from-string to-string t nil nil nil nil (region-beginning) (region-end))
       (perform-replace from-string to-string t nil nil))))
